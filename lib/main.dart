cf80fac07b6d2962a339020b82990e63a7a728ce
Jovani de Souza <jovanidesouza@hotmail.com>
2021-03-03 23:37:48 -0300

    feat: add file manager funcions

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:async';
+import 'dart:io';
+import 'dart:convert';
 
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List _toDoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  Future<File> _getFiile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFiile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFiile();
+      return file.readAsString();
+    } catch (e) {
+      return null;
+    }
+  }
+}
